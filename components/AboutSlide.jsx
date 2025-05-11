68ba34754df23649f29e017ede540a5b30e58b08
Akhlaqy19 <akhlaqymhmd96@gmail.com>
2025-05-11 22:54:22 +0330

    fix some issues

--- components/AboutSlide.jsx
+++ components/AboutSlide.jsx
@@ -5,7 +5,9 @@ const AboutSlide = ({ year, info }) => {
   return (
     <>
       <div className="text-white space-y-6">
-        <h3 className="text-left text-6xl">{year}</h3>
+        <h3 className="text-left text-xl md:text-2xl mid: lg:text-5xl">
+          {year}
+        </h3>
         <p className="max-w-68 text-lg text-stone-600">{info}</p>
       </div>
     </>
